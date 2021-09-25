a23d61dc2d98e197a15b406c2fe72ce1cec5296d
Barát Péter <79026925+BaratPeti@users.noreply.github.com>
2021-09-25 21:19:05 +0200

    Main
    hátha fut

---
+++ Raft/cmake-build-debug/CMakeFiles/Raft.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raft.dir/main.c.obj"
+  "Raft.exe"
+  "Raft.exe.manifest"
+  "Raft.pdb"
+  "libRaft.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Raft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Raft/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    //ha minden igaz akkor ezt már nektek is látnotok kell.
+    //egy kis próba képpen próbáljátomeg beírna a neveiteket ide:
+    //
+    //
+    //
+    return 0;
+}
